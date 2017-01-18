2480febf9e0fb47cdc3f86fb4177fa60dca5d3c7
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-18 10:03:33 +0100

    Exception thrown when a version string is not valid

---
+++ src/Exception/InvalidVersionException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Site\Installer\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Exception thrown when a version string is not valid.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class InvalidVersionException extends InvalidArgumentException
+{
+}
