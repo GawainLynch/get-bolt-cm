64f286d16eae867d520afbffba4586af5fb97f1c
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-18 16:01:53 +0100

    Exception for missing JSON file

---
+++ src/Exception/MissingVersionsJsonException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Site\Installer\Exception;
+
+use RuntimeException;
+
+/**
+ * Thrown when versions.json is missing.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class MissingVersionsJsonException extends RuntimeException
+{
+}
